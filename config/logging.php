b88a2c3c86ca552ba7694a05ad5d97891834d681
user <dmitr.fel@gmail.com>
2018-05-31 09:34:23 +0300

    logging configuration

---
+++ config/logging.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 30.05.18
+ * Time: 8:22
+ */
+
+return [
+    'channels' => [
+        'syslog' => [
+            'driver' => 'syslog',
+            'level' => 'debug',
+        ],
+    ],
+];
