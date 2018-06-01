31f396487ad13046737d6dd75b1f4716746b80a6
user <dmitr.fel@gmail.com>
2018-06-01 09:01:38 +0300

    IdeaController.php refactoring

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Idea;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Response;
 use Spatie\MediaLibrary\Media;
 use App\Services\Idea\IdeaManager;
 use App\Http\Requests\IdeaFillRequest;
@@ -17,40 +16,28 @@ class IdeaController extends Controller
 {
     /**
      * @param Idea $idea
+     * @param IdeaManager $ideaManager
      * @return \Illuminate\Http\JsonResponse
      */
-    public function get_stored_idea_files(Idea $idea)
+    public function get_stored_idea_files(Idea $idea, IdeaManager $ideaManager)
     {
-        $files = $idea->getMedia();
-
-        $files->transform(function (Media $media) use ($idea) {
-            $media->url = $idea->prepareMediaConversion($media);
-            return $media;
-        });
-
-        return Response::json($files->filter()->toArray(), 200);
+        $files = $ideaManager->ideaStoredFiles($idea);
+        return response()->json($files->filter()->toArray(), 200);
     }
 
     /**
      * @param Idea $idea
      * @param Request $request
+     * @param IdeaManager $ideaManager
      * @return \Illuminate\Http\JsonResponse
-     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileDoesNotExist
-     * @throws \Spatie\MediaLibrary\Exceptions\FileCannotBeAdded\FileIsTooBig
      */
-    public function prefill_meta_files(Idea $idea, Request $request)
+    public function prefill_meta_files(Idea $idea, Request $request, IdeaManager $ideaManager)
     {
         $files = collect();
-
-        if ($request->hasFile('files')) {
-            foreach ($request->files as $file) {
-                $media      = $idea->addMedia($file)->toMediaCollection();
-                $media->url = $idea->prepareMediaConversion($media);
-                $files->push($media);
-            }
+        if($request->has(files)) {
+            $files = $ideaManager->prefillMetaFiles($idea, $request->files);
         }
-
-        return Response::json($files, 200);
+        return response()->json($files, 200);
     }
 
     /**
@@ -61,17 +48,27 @@ class IdeaController extends Controller
     public function remove_stored_files(Idea $idea, Media $media)
     {
         $idea->media()->findOrFail($media->id)->delete();
-        return Response::json('success', 200);
+        return response()->json('success', 200);
     }
 
+    /**
+     * @param Idea $idea
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function show(Idea $idea)
     {
         return view('entity.idea.theme_fill', compact('idea'));
     }
 
+    /**
+     * @param IdeaManager $ideaManager
+     * @param IdeaFillRequest $request
+     * @param Idea $idea
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function update(IdeaManager $ideaManager, IdeaFillRequest $request, Idea $idea)
     {
-        $ideaManager->update($request, $idea);
+        $ideaManager->update($request->input(), $idea);
         return redirect()->route('projects.show', ['project' => $idea->project_id])->with('success', 'Idea has been successfully updated!');
     }
 }
--- app/Services/Idea/IdeaManager.php
+++ app/Services/Idea/IdeaManager.php
@@ -2,17 +2,56 @@
 
 namespace App\Services\Idea;
 
+use App\Models\Idea;
+use Spatie\MediaLibrary\Media;
+
+/**
+ * Class IdeaManager
+ * @package App\Services\Idea
+ */
 class IdeaManager
 {
-   public function update($request, $idea)
+    /**
+     * @param array $params
+     * @param Idea $idea
+     */
+    public function update(array $params, Idea $idea)
    {
-       $idea->article_format_type = $request->article_format_type;
-       $idea->link_to_model_article = $request->link_to_model_article;
-       $idea->references = $request->references;
-       $idea->points_covered = $request->points_covered;
-       $idea->points_avoid = $request->points_avoid;
-       $idea->additional_notes = $request->additional_notes;
-       $idea->completed = 1;
-       $idea->save();
+       $params['completed'] = 1;
+       $idea->update($params);
    }
+
+    /**
+     * @param Idea $idea
+     * @return \Illuminate\Support\Collection
+     */
+    public function ideaStoredFiles(Idea $idea)
+   {
+       $files = $idea->getMedia();
+       $files->transform(function (Media $media) use ($idea) {
+           $media->url = $idea->prepareMediaConversion($media);
+           return $media;
+       });
+       return $files;
+   }
+
+    /**
+     * @param Idea $idea
+     * @param $files
+     * @return \Illuminate\Http\JsonResponse|\Illuminate\Support\Collection
+     */
+    public function prefillMetaFiles(Idea $idea, $files)
+    {
+        $result = collect([]);
+        foreach ($files as $file) {
+            try {
+                $media = $idea->addMedia($file)->toMediaCollection();
+            } catch (\Exception $e) {
+                return response()->json('error', 500);
+            }
+            $media->url = $idea->prepareMediaConversion($media);
+            $result->push($media);
+        }
+        return $result;
+    }
 }
