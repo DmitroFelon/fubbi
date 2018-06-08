4c46f0927123a90a27d943d13d3fb9d236a438c3
user <dmitr.fel@gmail.com>
2018-06-08 16:30:20 +0300

    refactoring

--- app/Http/Controllers/Webhooks/ThrivecartController.php
+++ app/Http/Controllers/Webhooks/ThrivecartController.php
@@ -97,15 +97,16 @@ class ThrivecartController extends Controller
         }
         try {
             //stripe customer id
-            $customer_identifier = $request->input('customer_identifier');
+//            $customer_identifier = $request->input('customer_identifier');
             //form data
             $customer            = $request->input('customer');
             //subscription ids, get first
-            $subscription_id     = collect($request->input('subscriptions'))->first();
-            $plan_id             = config('fubbi.thrive_cart_plans')[$product_id];
-            $stripe_customer     = Customer::retrieve($customer_identifier);
-            $stripe_subscription = Subscription::retrieve($subscription_id);
-            $customer_card       = collect($stripe_customer->sources->data)->first();
+//            $subscription_id     = collect($request->input('subscriptions'))->first();
+//            $plan_id             = config('fubbi.thrive_cart_plans')[$product_id];
+//            $stripe_customer     = Customer::retrieve($customer_identifier);
+            $stripe_subscription = Subscription::retrieve(collect($request->input('subscriptions'))->first());
+//            $customer_card       = collect($stripe_customer->sources->data)->first();
+            $customer_card       = collect(Customer::retrieve($request->input('customer_identifier'))->sources->data)->first();
             $user                = User::whereEmail($customer['email'] ?? '')->first();
             if (!$user) {
                 $tmp_password = str_random(8);
@@ -127,9 +128,9 @@ class ThrivecartController extends Controller
                     Log::error($e);
                 }
             }
-            $params['plan_id'] = $plan_id;
+            $params['plan_id'] = config('fubbi.thrive_cart_plans')[$product_id];
             $params['project_name'] = $customer['business_name'] ?? 'Project #' . strval($request->input('order_id'));
-            $params['stripeToken'] = $customer_identifier;
+            $params['stripeToken'] = $request->input('customer_identifier');
             $subscriptionManager->subscriptionCreate($user, $this->project, $params, ProjectStates::QUIZ_FILLING);
         } catch (\Exception $e) {
             Log::error($e);
