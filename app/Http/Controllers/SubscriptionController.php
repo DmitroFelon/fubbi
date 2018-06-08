1231521f7ed010cbb9c15c45c6d1cb041d8f1093
user <dmitr.fel@gmail.com>
2018-06-08 09:05:21 +0300

    subscriptionController refactoring

--- app/Http/Controllers/SubscriptionController.php
+++ app/Http/Controllers/SubscriptionController.php
@@ -4,50 +4,20 @@ namespace App\Http\Controllers;
 
 use App\Models\Helpers\ProjectStates;
 use App\Models\Project;
+use App\Services\Subscription\SubscriptionManager;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
 
 class SubscriptionController extends Controller
 {
 
-    public function __invoke(Request $request, Project $project)
+    public function __invoke(Request $request, Project $project, SubscriptionManager $subscriptionManager)
     {
-        try {
-            $plan_id = $request->input('plan_id');
-
-            //create subscription
-            $subscription = Auth::user()
-                                ->newSubscription(
-                                    $request->input('project_name'),
-                                    $plan_id)
-                                ->create($request->input('stripeToken'));
-            //attach client id
-            $project->client_id = Auth::user()->id;
-            //set name
-            $project->name = $request->input('project_name');
-            //attach subscription
-            $project->subscription_id = $subscription->id;
-            //set state
-            $project->setState(ProjectStates::QUIZ_FILLING);
-            $project->save();
-
-            //fill services
-            $project->setServices($plan_id);
-            //set filrst cycle
-            $project->setCycle($plan_id);
-
-            //set quiz flag in case user will click "back" button
-            Session::put('quiz', $project->id);
-
-            return redirect()
-                ->action('Resources\ProjectController@edit', [$project, 's' => ProjectStates::QUIZ_FILLING])
-                ->with('success', _i('Your subscribtion has been created successfully'));
-
-        } catch (\Exception $e) {
-            redirect()->back()
-                      ->with('error', _i('Something wrong happened while your subscription, please contact to administrator'));
+        $data = $subscriptionManager->subscriptionCreate($request->user(), $project, $request->input(), ProjectStates::QUIZ_FILLING);
+        if($data['error'] != '') {
+            return redirect()->back()->with('error', $data['error']);
         }
+        return redirect()
+            ->action('Resources\ProjectController@edit', [$data['project'], 's' => ProjectStates::QUIZ_FILLING])
+            ->with('success', _i('Your subscription has been created successfully'));
     }
-
 }
---
+++ app/Services/Subscription/SubscriptionManager.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 08.06.18
+ * Time: 8:35
+ */
+
+namespace App\Services\Subscription;
+
+use App\Models\Project;
+use App\User;
+use Illuminate\Support\Facades\Session;
+
+class SubscriptionManager
+{
+    public function subscriptionCreate(User $user, Project $project, array  $params, $state)
+    {
+        $data['error'] = '';
+        $plan_id = $params['plan_id'];
+        $subscription = $user->newSubscription($params['project_name'], $plan_id)->create($params['stripeToken']);
+        $project->client_id = $user->id;
+        $project->name = $params['project_name'];
+        $project->subscription_id = $subscription->id;
+        try {
+            $project->setState($state);
+        } catch (\Exception $e) {
+            $data['error'] = $e->getMessage();
+            return $data;
+        }
+        $project->save();
+        try {
+            $project->setServices($plan_id);
+        } catch (\Exception $e) {
+            $data['error'] = $e->getMessage();
+            return $data;
+        }
+        $project->setCycle($plan_id);
+        //set quiz flag in case user will click "back" button
+        Session::put('quiz', $project->id);
+        $data['project'] = $project;
+        return $project;
+    }
+}
